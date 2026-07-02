88df2728e419a3c34e634df2a7a7fc891f7ed36d
Vikram77727 <vikram200577@gmail.com>
2026-07-02 12:25:01 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Vikram_Protfolio/',
+  base: '/Vikram_Portfolio/',
 })
